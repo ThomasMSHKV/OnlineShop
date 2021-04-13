1effe77b6a8227dbf658591b29becb5f361a41eb
OlegDeveloper <tmereshkov@gmail.com>
2021-04-13 21:30:05 +0300

    started

---
+++ src/main/java/com/example/onlineshop/network/Entities.kt
@@ -0,0 +1,20 @@
+package com.example.onlineshop.network
+
+import com.google.gson.annotations.SerializedName
+
+data class Products(
+    @SerializedName("data") val products: List<Product>
+
+)
+
+data class Product(
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("short_description") val short_description: String,
+    @SerializedName("image_url") val image_url: String,
+    @SerializedName("amount") val amount: Int,
+    @SerializedName("price") val price: Double,
+    @SerializedName("producer") val producer: String,
+    @SerializedName("categories") val categories: String
+
+)
